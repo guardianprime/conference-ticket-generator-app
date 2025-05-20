e90e0eaea5801342627c8d82b44f1ce266118634
guardianprime <okongordian@gmail.com>
2025-05-20 14:43:59 -0700

    chore: mobile background styling update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/logo-full.svg"
 
 function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
     return (
-        <form onSubmit={handleSubmit} className="bg-indigo-950 min-h-screen max-h-max p-4 w-full">
+        <form onSubmit={handleSubmit} className="bg-multi min-h-screen max-h-max p-4 w-full">
             <div className="flex justify-center items-center w-full h-10">
                 <img src={logo} alt="coding conf logo" />
             </div>
@@ -10,7 +10,7 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
             <p className="text-neutral-100 text-sm text-center">Secure your spot at next year's biggest coding conference.</p>
             <div>
                 <label htmlFor="dropzone-file" className="text-white"> Upload Avatar</label>
-                <div className="flex items-center justify-center w-full my-2 bg-indigo-950 border-2 border-white border-dashed rounded-lg">
+                <div className="flex items-center justify-center w-full my-2 bg-indigo-950 border-2 border-neutral-300 border-dashed rounded-lg">
                     {!avatarPreview && <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-30 cursor-pointer ">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                             <div className="rounded-lg bg-indigo-900 h-12 w-12 flex justify-center items-center p-1">
@@ -28,25 +28,25 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
                         </div>
                         <div className="flex justify-between w-full">
                             <button className="p-1 text-white text-xs bg-cyan-200 rounded-lg" onClick={(e) => handleReset(e)}>Remove image</button>
-                            <button className="p-1 text-white text-xs bg-cyan-200 rounded-lg">Change image</button>
+                            <button className="p-1 text-white text-xs bg-cyan-200 rounded-lg" onClick={() => console.log("changing image")}>Change image</button>
                         </div>
                     </div>}
                 </div>
                 <p className="text-white text-xs">Upload your photo (JPG or PNG, max size: 500KB).</p>
             </div>
-            <div>
+            <div className="mt-4">
                 <label htmlFor="userName" className="text-white">Full Name</label>
-                <input className="border border-white p-2 my-2 w-full text-white rounded-sm" id="userName" name="userName" type="text" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userName" name="userName" type="text" onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="userEmail" className="text-white">Email Address</label>
-                <input className="border border-white p-2 my-2 w-full text-white rounded-sm" id="userEmail" name="userEmail" placeholder="example@email.com" type="email" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userEmail" name="userEmail" placeholder="example@email.com" type="email" onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="userGithubName" className="text-white">GitHub Username</label>
-                <input className="border border-white p-2 my-2 w-full text-white rounded-sm" id="userGithubName" name="userGithubName" type="text" placeholder="@yourusername" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userGithubName" name="userGithubName" type="text" placeholder="@yourusername" onChange={handleChange} />
             </div>
-            <button type="submit" className="bg-orange-500 text-black font-semibold p-4 w-full my-5 rounded-sm">Generate My Ticket</button>
+            <button type="submit" className="bg-[#f57261] text-black font-bold p-4 w-full my-5 rounded-lg">Generate My Ticket</button>
         </form>
     )
 }
