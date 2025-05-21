4d538b81b81dc798bee7ef6f941027df459331af
guardianprime <okongordian@gmail.com>
2025-05-21 03:16:57 -0700

    chore: updated the input bg color

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,10 +12,10 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
             <p className="text-neutral-100 text-sm text-center">Secure your spot at next year's biggest coding conference.</p>
             <div className="mt-6">
                 <label htmlFor="dropzone-file" className="text-white"> Upload Avatar</label>
-                <div className="flex items-center justify-center w-full my-2 bg-indigo-950 border-2 border-neutral-300 border-dashed rounded-lg">
+                <div className="flex items-center justify-center w-full my-2 bg-indigo-950/90 border-2 border-neutral-300 border-dashed rounded-lg">
                     {!avatarPreview && <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-30 cursor-pointer ">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <div className="rounded-lg bg-indigo-900 h-12 w-12 flex justify-center items-center p-1">
+                            <div className="rounded-lg bg-indigo-950/95 h-12 w-12 flex justify-center items-center p-1 border border-neutral-300/50">
                                <img src={UploadIcon} alt="upload icon"/>
                             </div>
                             <p className="mt-2 text-sm text-white "><span className="font-semibold">Click to upload</span> or drag and drop</p>
@@ -37,15 +37,15 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
             </div>
             <div className="mt-4">
                 <label htmlFor="userName" className="text-white">Full Name</label>
-                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userName" name="userName" type="text" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950/90" id="userName" name="userName" type="text" onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="userEmail" className="text-white">Email Address</label>
-                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userEmail" name="userEmail" placeholder="example@email.com" type="email" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950/90" id="userEmail" name="userEmail" placeholder="example@email.com" type="email" onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="userGithubName" className="text-white">GitHub Username</label>
-                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950" id="userGithubName" name="userGithubName" type="text" placeholder="@yourusername" onChange={handleChange} />
+                <input className="border border-neutral-300 p-2 my-2 w-full text-white rounded-lg bg-indigo-950/90" id="userGithubName" name="userGithubName" type="text" placeholder="@yourusername" onChange={handleChange} />
             </div>
             <button type="submit" className="bg-[#f57261] text-black font-bold p-4 w-full my-5 rounded-lg">Generate My Ticket</button>
         </form>
