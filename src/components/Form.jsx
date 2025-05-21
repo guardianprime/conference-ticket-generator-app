a3f4007e4fab63896c832a341ea11ab792455026
guardianprime <okongordian@gmail.com>
2025-05-21 02:48:33 -0700

    fix: disappearing icons now visible

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,6 @@
 import logo from "../assets/images/logo-full.svg"
+import UploadIcon  from '../assets/images/icon-upload.svg';
+import infoIcon from '../assets/images/icon-info.svg';
 
 function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
     return (
@@ -14,9 +16,7 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
                     {!avatarPreview && <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-30 cursor-pointer ">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                             <div className="rounded-lg bg-indigo-900 h-12 w-12 flex justify-center items-center p-1">
-                                <svg className="w-8 h-8 text-orange-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                </svg>
+                               <img src={UploadIcon} alt="upload icon"/>
                             </div>
                             <p className="mt-2 text-sm text-white "><span className="font-semibold">Click to upload</span> or drag and drop</p>
                         </div>
@@ -32,7 +32,8 @@ function Form({ handleSubmit, handleChange, avatarPreview, handleReset }) {
                         </div>
                     </div>}
                 </div>
-                <p className="text-white text-xs">Upload your photo (JPG or PNG, max size: 500KB).</p>
+                <p className="text-white text-xs">
+                    <img alt="info icon" src={infoIcon} className="inline"/> Upload your photo (JPG or PNG, max size: 500KB).</p>
             </div>
             <div className="mt-4">
                 <label htmlFor="userName" className="text-white">Full Name</label>
