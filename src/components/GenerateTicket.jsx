ded3be0506e75bf01537447360b8775aa067e391
guardianprime <okongordian@gmail.com>
2025-05-22 01:43:35 -0700

    chore: started styling the generate form component

--- src/components/GenerateTicket.jsx
+++ src/components/GenerateTicket.jsx
@@ -1,14 +1,18 @@
+import logo from "../assets/images/logo-full.svg"
+
 function GenerateTicket({ formData }) {
     return (
-        <div>
+        <div className="bg-multi min-h-screen max-h-max p-4 w-full text-white">
+            <div className="flex justify-center items-center w-full h-10">
+                <img src={logo} alt="coding conf logo" />
+            </div>
+            <h1 className="text-4xl mt-4"> Congrats, <span>{formData.userName}</span> Your ticket is ready.</h1>
 
-            <h1> Congrats,  {formData.userName}   Your ticket is ready.</h1>
-
-            <p> We've emailed your ticket to {formData.userEmail} and will send updates in the run up to the event.</p>
-
+            <p className="mt-3"> We've emailed your ticket to <span>{formData.userEmail}</span> and will send updates in the run up to the event.</p>
+            <div className="mt-8">
             Coding Conf
             Jan 31, 2025 / Austin, TX
-
+            </div>
         </div>
     )
 }
