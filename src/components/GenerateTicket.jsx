0a3a2b9940a65bcd9fdbde1f9ba6bdf9d9fca0bd
guardianprime <okongordian@gmail.com>
2025-07-26 12:23:26 +0100

    chore: adjusted the ticket like content styles

--- src/components/GenerateTicket.jsx
+++ src/components/GenerateTicket.jsx
@@ -5,17 +5,32 @@ import cardDesign from "../assets/images/pattern-ticket.svg";
 function GenerateTicket({ formData, avatarPreview }) {
   return (
     <div className="bg-multi min-h-screen max-h-max p-4 w-full text-white">
-      <div className="flex justify-center items-center w-full h-10">
+      <div className="flex justify-between items-center w-2/5 h-10 mx-auto">
         <img src={logo} alt="coding conf logo" />
+        <h2 className="text-lg"> Coding Conf</h2>
       </div>
-      <h1 className="text-4xl mt-4">
-        Congrats, <span>{formData.userName}</span> Your ticket is ready.
+      <h1 className="text-4xl mt-4 text-center">
+        Congrats,{" "}
+        {formData.userName?.split(" ").length === 2 ? (
+          <>
+            <span className="bg-gradient-to-r from-[#f57261] to-white bg-clip-text text-transparent">
+              {formData.userName.split(" ")[0]}
+            </span>{" "}
+            <span className="bg-gradient-to-r from-[#f57261] to-white bg-clip-text text-transparent">
+              {formData.userName.split(" ")[1]}
+            </span>
+          </>
+        ) : (
+          <span className="text-[#f57261]">{formData.userName}</span>
+        )}
+        ! Your ticket is ready.
       </h1>
-      <p className="mt-3">
-        We've emailed your ticket to <span>{formData.userEmail}</span> and will
+      <p className="mt-3 text-center">
+        We've emailed your ticket to{" "}
+        <span className="text-[#f57261]">{formData.userEmail}</span> and will
         send updates in the run up to the event.
       </p>
-      <div className=" w-full h-50 rounded-md mt-6 relative p-3">
+      <div className=" w-full h-50 rounded-md mt-8 relative p-3">
         <div className="w-full h-full absolute bottom-0 left-0">
           <img src={cardDesign} alt="card design" />
         </div>
